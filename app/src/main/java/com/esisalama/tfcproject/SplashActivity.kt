9e4421d4c1b7bb3637add96a1de4c997ffdfdbd0
ITOT <ngalukiyemaureen@gmail.com>
2019-08-12 10:28:09 +0200

    remplacer image1 par splash et correction de fichier xml

--- app/src/main/java/com/esisalama/tfcproject/SplashActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/SplashActivity.kt
@@ -4,26 +4,24 @@ import android.content.Intent
 import android.os.Bundle
 import android.os.Handler
 import androidx.appcompat.app.AppCompatActivity
-import java.lang.reflect.InvocationHandler
+import androidx.core.content.ContextCompat.startActivity
 
-class SplashActivity :AppCompatActivity(){
-    private val splashTime =  3000L
-    private lateinit var myHandler: Handler
 
-    override fun onCreate(savedInstanceState:Bundle?){
+class SplashActivity:AppCompatActivity() {
+    private val splashTime= 3000L
+    private  lateinit var myHandler: Handler
+
+    override fun onCreate(savedInstanceState: Bundle?){
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash)
 
-        myHandler = Handler()
-
-        myHandler.postDelayed({
-            goToMainActivity()
-        },splashTime)
+        myHandler= Handler()
+        myHandler.postDelayed({goToMainActivity()},splashTime)
     }
+
     private fun goToMainActivity(){
-        val mainActivityIntent= Intent(applicationContext,MainActivity::class.java)
+        val mainActivityIntent = Intent(applicationContext,MainActivity::class.java)
         startActivity(mainActivityIntent)
         finish()
     }
-
 }
