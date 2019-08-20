84beb29a9a6a94e0bcacecd31841e76dcaa526a1
Eric Ampire <ericampire.top@gmail.com>
2019-08-20 21:35:59 +0200

    Overriding the right method

--- app/src/main/java/com/esisalama/tfcproject/LoginActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/LoginActivity.kt
@@ -7,8 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_login.*
 
 class LoginActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
 
         btnLoginRegister.setOnClickListener {
--- app/src/main/java/com/esisalama/tfcproject/RegisterActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/RegisterActivity.kt
@@ -7,8 +7,8 @@ import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_register.*
 
 class RegisterActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
-        super.onCreate(savedInstanceState, persistentState)
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_register)
 
         btnRegisterLogin.setOnClickListener {
