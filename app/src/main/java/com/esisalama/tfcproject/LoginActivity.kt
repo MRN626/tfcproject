7fc5a2b8d1efe0f0dea5d59f788e8cc09bbdc9c2
Eric Ampire <ericampire.top@gmail.com>
2019-08-20 20:35:28 +0200

    Login navigation

--- app/src/main/java/com/esisalama/tfcproject/LoginActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/LoginActivity.kt
@@ -1,4 +1,18 @@
 package com.esisalama.tfcproject
 
-class LoginActivity {
+import android.content.Intent
+import android.os.Bundle
+import android.os.PersistableBundle
+import androidx.appcompat.app.AppCompatActivity
+import kotlinx.android.synthetic.main.activity_login.*
+
+class LoginActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?, persistentState: PersistableBundle?) {
+        super.onCreate(savedInstanceState, persistentState)
+        setContentView(R.layout.activity_login)
+
+        btnLoginRegister.setOnClickListener {
+            startActivity(Intent(this, RegisterActivity::class.java))
+        }
+    }
 }
