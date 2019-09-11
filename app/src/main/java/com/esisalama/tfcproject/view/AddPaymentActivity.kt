8ff3eb89ebf1585e9f6855202061b0f1aab8a0a0
Eric Ampire <ericampire.top@gmail.com>
2019-09-11 15:40:46 +0200

    Add AddPaymentActivity.kt

---
+++ app/src/main/java/com/esisalama/tfcproject/view/AddPaymentActivity.kt
@@ -0,0 +1,13 @@
+package com.esisalama.tfcproject.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.esisalama.tfcproject.R
+
+class AddPaymentActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_payment)
+    }
+}
