f11b99161073ddc994063c6fc7a933a3fd7d0480
Eric Ampire <ericampire.top@gmail.com>
2019-09-13 12:08:54 +0200

    Adding Scan Product activity

---
+++ app/src/main/java/com/esisalama/tfcproject/view/ScanProductActivity.kt
@@ -0,0 +1,124 @@
+package com.esisalama.tfcproject.view
+
+import android.Manifest
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
+import androidx.lifecycle.ViewModelProviders
+import androidx.lifecycle.get
+import com.esisalama.tfcproject.R
+import com.esisalama.tfcproject.model.Product
+import com.esisalama.tfcproject.model.ProductCart
+import com.esisalama.tfcproject.viewModel.ProductViewModel
+import com.google.gson.GsonBuilder
+import com.google.zxing.BarcodeFormat
+import com.google.zxing.Result
+import com.kinda.alert.KAlertDialog
+import kotlinx.android.synthetic.main.activity_scanne_product.*
+import me.dm7.barcodescanner.zxing.ZXingScannerView
+import java.util.*
+import kotlin.properties.Delegates
+
+class ScanProductActivity : AppCompatActivity(), ZXingScannerView.ResultHandler {
+
+    private var dialog: KAlertDialog by Delegates.notNull()
+    private val mViewModel by lazy {
+        ViewModelProviders.of(this).get<ProductViewModel>()
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_scanne_product)
+        setScannerProperties()
+
+        btnLaunchScanner.setOnClickListener {
+            reloadScanner()
+        }
+    }
+
+    private fun reloadScanner() {
+        qrCodeScanner.run {
+            stopCamera()
+            startCamera()
+            setResultHandler(this@ScanProductActivity)
+        }
+    }
+
+    override fun handleResult(result: Result?) {
+        if (result != null) {
+            dialog = KAlertDialog(this, KAlertDialog.PROGRESS_TYPE).apply {
+                titleText = "Verification !"
+                contentText = "Verification en cour d'execution..."
+                show()
+            }
+
+            dialog.setConfirmClickListener {
+                if (it.alerType == KAlertDialog.WARNING_TYPE || it.alerType == KAlertDialog.ERROR_TYPE) {
+                    it.dismissWithAnimation()
+                    reloadScanner()
+                }
+            }
+
+            getDataFromQrCode(result)
+        }
+    }
+
+    private fun getDataFromQrCode(result: Result) {
+        val productJson = result.text.toString()
+        val gson = GsonBuilder().create()
+
+        try {
+            val product = gson.fromJson(productJson, Product::class.java)
+            val productCart = ProductCart(
+                uid = UUID.randomUUID().toString(),
+                product = product,
+                quantity = 1
+            )
+
+            mViewModel.add(productCart)
+            startActivity(Intent(this, AddPaymentActivity::class.java))
+            finish()
+
+        } catch (e: Exception) {
+            dialog.changeAlertType(KAlertDialog.ERROR_TYPE)
+            dialog.titleText = "Erreur"
+            dialog.contentText = ""
+        }
+    }
+
+    override fun onPause() {
+        super.onPause()
+        qrCodeScanner.stopCamera()
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            if (ContextCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+
+                ActivityCompat
+                    .requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 1)
+                return
+            }
+        }
+
+        qrCodeScanner.run {
+            startCamera()
+            setResultHandler(this@ScanProductActivity)
+        }
+    }
+
+    private fun setScannerProperties() {
+        qrCodeScanner.run {
+            setFormats(listOf(BarcodeFormat.QR_CODE))
+            setAutoFocus(true)
+            setLaserColor(R.color.colorAccent)
+            setMaskColor(R.color.colorAccent)
+        }
+    }
+}
