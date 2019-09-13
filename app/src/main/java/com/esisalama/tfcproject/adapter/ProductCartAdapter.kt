249adb0223a4f605dfbc2977ab335d4960e23cba
Eric Ampire <ericampire.top@gmail.com>
2019-09-13 10:52:00 +0200

    Refactoring

--- app/src/main/java/com/esisalama/tfcproject/adapter/ProductCartAdapter.kt
+++ app/src/main/java/com/esisalama/tfcproject/adapter/ProductCartAdapter.kt
@@ -4,12 +4,17 @@ import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.recyclerview.widget.DiffUtil
 import androidx.recyclerview.widget.ListAdapter
+import com.amulyakhare.textdrawable.util.ColorGenerator
 import com.esisalama.tfcproject.adapter.common.CustomClick
 import com.esisalama.tfcproject.adapter.common.CustomViewHolder
 import com.esisalama.tfcproject.databinding.ItemProductCartBinding
 import com.esisalama.tfcproject.model.ProductCart
 
-class ProductCartAdapter(val listener: CustomClick<ProductCart>) : ListAdapter<ProductCart, CustomViewHolder>(Companion) {
+class ProductCartAdapter(private val listener: CustomClick<ProductCart>) :
+    ListAdapter<ProductCart, CustomViewHolder>(Companion) {
+
+    private val colorGenerator = ColorGenerator.MATERIAL
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): CustomViewHolder {
         val inflater = LayoutInflater.from(parent.context)
         val binding = ItemProductCartBinding.inflate(inflater, parent, false)
@@ -21,9 +26,12 @@ class ProductCartAdapter(val listener: CustomClick<ProductCart>) : ListAdapter<P
         val currentItem = getItem(position) ?: return
         val productCartBinding = holder.binding as ItemProductCartBinding
 
-        productCartBinding.productCart = currentItem
-        productCartBinding.btnMore.setOnClickListener {
-            listener.onItemClick(currentItem, productCartBinding.root)
+        productCartBinding.run {
+            view.setBackgroundColor(colorGenerator.getColor(currentItem))
+            productCart = currentItem
+            btnMore.setOnClickListener {
+                listener.onItemClick(currentItem, productCartBinding.root)
+            }
         }
     }
 
