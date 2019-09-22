76ead6f220bf4c9d61cbbcf4a7953ffea956574d
Eric Ampire <ericampire.top@gmail.com>
2019-09-22 18:26:29 +0200

    Adding NFC reader and writer logic

--- app/src/main/java/com/esisalama/tfcproject/view/AddPaymentActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/view/AddPaymentActivity.kt
@@ -1,27 +1,41 @@
 package com.esisalama.tfcproject.view
 
+import android.app.PendingIntent
 import android.content.Intent
+import android.content.IntentFilter
+import android.nfc.NfcAdapter
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.PopupMenu
 import android.widget.Toast
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.ActionBar
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.lifecycle.ViewModelProviders
 import androidx.lifecycle.get
+import com.esisalama.tfcproject.BuildConfig
 import com.esisalama.tfcproject.R
 import com.esisalama.tfcproject.adapter.ProductCartAdapter
 import com.esisalama.tfcproject.adapter.common.CustomClick
 import com.esisalama.tfcproject.databinding.ActivityAddPaymentBinding
+import com.esisalama.tfcproject.model.Operation
 import com.esisalama.tfcproject.model.ProductCart
+import com.esisalama.tfcproject.util.readFromNfc
+import com.esisalama.tfcproject.util.writeNfc
 import com.esisalama.tfcproject.viewModel.ProductViewModel
+import com.google.firebase.auth.FirebaseAuth
+import com.google.gson.GsonBuilder
+import org.jetbrains.anko.longToast
 
 class AddPaymentActivity : AppCompatActivity(), CustomClick<ProductCart> {
 
+    private var stringJson: String? = null
+    private var mNfcAdapter: NfcAdapter? = null
     private val binding by lazy {
         DataBindingUtil.setContentView<ActivityAddPaymentBinding>(this, R.layout.activity_add_payment)
     }
@@ -42,12 +56,77 @@ class AddPaymentActivity : AppCompatActivity(), CustomClick<ProductCart> {
 
         binding.viewModel = mViewModel
         binding.adapter = productCartAdapter
+        setupNfcAdapter()
+    }
+
+    private fun setupNfcAdapter() {
+        mNfcAdapter = NfcAdapter.getDefaultAdapter(this)
+        val isAvailable = mNfcAdapter != null && mNfcAdapter!!.isEnabled
+        if (isAvailable) {
+            Toast.makeText(this, "NFC disponible", Toast.LENGTH_LONG).show()
+        } else {
+            Toast.makeText(this, "NFC non-disponible", Toast.LENGTH_LONG).show()
+        }
     }
 
     fun finishAction(view: View) {
-        // Todo: Save data in nfc tag
-        // Todo: remove data
-        // Todo: Show loading
+        val gson = GsonBuilder()
+            .setPrettyPrinting()
+            .create()
+
+        mViewModel.productCart.observe(this, Observer {
+            if (it.isNotEmpty()) {
+                Log.e("finish", "isis")
+                val accountNumber = FirebaseAuth.getInstance().currentUser?.uid ?: return@Observer
+                val operation = Operation(accountNumber, it)
+                stringJson = gson.toJson(operation)
+                Toast.makeText(baseContext, "Rapprocher la puce pour enregistre les infos", Toast.LENGTH_LONG).show()
+            } else {
+                Toast.makeText(baseContext, "Aucune informations", Toast.LENGTH_LONG).show()
+            }
+        })
+    }
+
+    override fun onResume() {
+        super.onResume()
+
+        mNfcAdapter ?: return
+
+        // Dispatch les intents à l'activity
+        val intent = Intent(this, this::class.java).addFlags(Intent.FLAG_RECEIVER_REPLACE_PENDING)
+        val pendingIntent = PendingIntent.getActivity(this, 0, intent, 0)
+        val intentFilter = arrayOf<IntentFilter>()
+        mNfcAdapter?.enableForegroundDispatch(this, pendingIntent, intentFilter, null)
+    }
+
+    @RequiresApi(Build.VERSION_CODES.LOLLIPOP)
+    override fun onNewIntent(intent: Intent) {
+        Log.e("ericampire", "${intent.action}")
+        val tagAction = intent.action
+        if (NfcAdapter.ACTION_NDEF_DISCOVERED == tagAction || NfcAdapter.ACTION_TAG_DISCOVERED == tagAction) {
+            when (BuildConfig.FLAVOR) {
+                "producer" -> {
+                    Log.e("ericampire", "producer")
+                    if (stringJson == null) {
+                        longToast("Appuyer sur terminer l'enregistrement")
+                    } else {
+                        if (writeNfc(intent, stringJson!!)) {
+                            Toast.makeText(baseContext, "Saved", Toast.LENGTH_LONG).show()
+                            mViewModel.removeAll()
+                            stringJson = null
+
+                        } else {
+                            Log.e("ericampire", "failed to write information")
+                        }
+                    }
+                }
+                else -> {
+                    Log.e("ericampire", "else")
+                }
+            }
+        }
+
+        super.onNewIntent(intent)
     }
 
     fun addAction(view: View) {
