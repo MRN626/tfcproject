875098927d7f439547e4206a68d02888e095d740
Eric Ampire <ericampire.top@gmail.com>
2019-10-13 17:25:56 +0200

    Loading state

--- app/src/main/java/com/esisalama/tfcproject/view/HistoriqueTransactionActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/view/HistoriqueTransactionActivity.kt
@@ -1,8 +1,10 @@
 package com.esisalama.tfcproject.view
 
+import android.app.ProgressDialog
 import android.content.Intent
 import android.os.Bundle
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
@@ -12,6 +14,7 @@ import com.esisalama.tfcproject.adapter.HistoriqueOperationAdapter
 import com.esisalama.tfcproject.adapter.common.CustomClick
 import com.esisalama.tfcproject.databinding.ActivityHistoriqueTransactionBinding
 import com.esisalama.tfcproject.model.HistoriqueOperation
+import com.esisalama.tfcproject.util.LoadingState
 import com.esisalama.tfcproject.viewModel.HistoriqueTransactionViewModel
 
 class HistoriqueTransactionActivity : AppCompatActivity() {
@@ -38,6 +41,29 @@ class HistoriqueTransactionActivity : AppCompatActivity() {
 
         mViewModel.historique.observe(this, Observer {
             historiqueTransactionAdapter.submitList(it)
+        })
+
+        val progressDialog = ProgressDialog(this).apply {
+            setTitle("Loading")
+            setCancelable(true)
+            setMessage("Recuperation de l'historique")
+        }
+
+        mViewModel.loadingState.observe(this, Observer {
+            when (it.status) {
+                LoadingState.Status.RUNNING -> {
+                    progressDialog.show()
+                }
+
+                LoadingState.Status.FAILED -> {
+                    progressDialog.hide()
+                    Toast.makeText(this, it.msg, Toast.LENGTH_LONG).show()
+                }
+
+                LoadingState.Status.SUCCESS -> {
+                    progressDialog.hide()
+                }
+            }
         })
 
         binding.adapter = historiqueTransactionAdapter
