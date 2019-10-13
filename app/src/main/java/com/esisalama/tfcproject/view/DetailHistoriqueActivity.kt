8763350099f8ba89217c4324ab6c8767c4a9432b
Eric Ampire <ericampire.top@gmail.com>
2019-10-13 14:59:11 +0200

    Adding DetailHistoriqueActivity.kt

---
+++ app/src/main/java/com/esisalama/tfcproject/view/DetailHistoriqueActivity.kt
@@ -0,0 +1,13 @@
+package com.esisalama.tfcproject.view
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.esisalama.tfcproject.R
+
+class DetailHistoriqueActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail_historique)
+    }
+}
