a4b467eb2e76e8acc671850124827990eb79a903
Eric Ampire <ericampire.top@gmail.com>
2019-10-13 14:59:38 +0200

    ViewModel logic and navigation to the Detail activity

--- app/src/main/java/com/esisalama/tfcproject/view/HistoriqueTransactionActivity.kt
+++ app/src/main/java/com/esisalama/tfcproject/view/HistoriqueTransactionActivity.kt
@@ -1,13 +1,45 @@
 package com.esisalama.tfcproject.view
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import androidx.lifecycle.Observer
+import androidx.lifecycle.ViewModelProviders
 import com.esisalama.tfcproject.R
+import com.esisalama.tfcproject.adapter.HistoriqueOperationAdapter
+import com.esisalama.tfcproject.adapter.common.CustomClick
+import com.esisalama.tfcproject.databinding.ActivityHistoriqueTransactionBinding
+import com.esisalama.tfcproject.model.HistoriqueOperation
+import com.esisalama.tfcproject.viewModel.HistoriqueTransactionViewModel
 
 class HistoriqueTransactionActivity : AppCompatActivity() {
 
+    private val binding by lazy {
+        DataBindingUtil.setContentView<ActivityHistoriqueTransactionBinding>(this, R.layout.activity_historique_transaction)
+    }
+
+    private val mViewModel by lazy {
+        ViewModelProviders.of(this).get(HistoriqueTransactionViewModel::class.java)
+    }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_historique_transaction)
+
+        val historiqueTransactionAdapter = HistoriqueOperationAdapter(object : CustomClick<HistoriqueOperation> {
+            override fun onItemClick(item: HistoriqueOperation, view: View) {
+                // Show detail transaction
+                val detailIntent = Intent(this@HistoriqueTransactionActivity, DetailHistoriqueActivity::class.java)
+                detailIntent.putExtra("operation", item)
+                startActivity(detailIntent)
+            }
+        })
+
+        mViewModel.historique.observe(this, Observer {
+            historiqueTransactionAdapter.submitList(it)
+        })
+
+        binding.adapter = historiqueTransactionAdapter
     }
 }
